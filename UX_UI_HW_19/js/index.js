d5eddef5da365c38ca8d4a50a59c4757ab35c3a2
Briana-B <80088533+Briana-B@users.noreply.github.com>
2021-09-02 13:19:42 -0600

    Hw20

---
+++ UX_UI_HW_19/js/index.js
@@ -0,0 +1,22 @@
+console.log("Your index.js file is loaded correctly!");
+
+
+
+function myFunction (){
+    document.getElementById(".dropdown").classList.toggle("show");
+}
+
+window.onclick= function(event) {
+    if (!event.target.matches('.hamburger')){
+        var dropdowns= document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns [i];
+            if (openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
